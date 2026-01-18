d4f34294e31032a4ffed9e6e075ab0144174600a
unknown <you@example.com>
2026-01-18 16:59:50 +0600

    add many code

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Features from '@/pages/Features';
 import Hero from '@/pages/Hero';
 import ImageSection from '@/pages/ImageSection';
 import React from 'react';
+import BlogSection from './blog/page';
 
 const page = () => {
   return (
@@ -9,6 +10,7 @@ const page = () => {
       <Hero></Hero>
       <Features></Features>
       <ImageSection></ImageSection>
+      <BlogSection></BlogSection>
     </div>
   );
 };
